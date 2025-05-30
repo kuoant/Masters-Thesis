afa7fb5984ce786bc9e774cdf630dba52b833ef8
kuoant <fabian.kuonen@outlook.com>
2025-05-30 15:12:26 +0200

    Add and update thesis files

---
+++ Python/ETL Process/install.sh
@@ -0,0 +1,11 @@
+
+psql -U postgres -f /files/files_2020_spring/northwind.postgre_pretest.sql
+psql -U postgres -f /files/files_2020_fall/create_examdb.sql
+psql -U postgres -f /files/files_2021_spring/schema.sql
+psql -U postgres -f /files/files_2021_spring/inserts.sql
+psql -U postgres -f /files/files_2022_fall/sqlfile.sql
+psql -U postgres -f /files/files_2022_spring/schema.sql
+psql -U postgres -f /files/files_2022_spring/airports.sql
+psql -U postgres -f /files/files_2022_spring/airlines.sql
+psql -U postgres -f /files/files_2022_spring/flights.sql
+psql -U postgres -f /files/files_2022_spring/flights_delays.sql
